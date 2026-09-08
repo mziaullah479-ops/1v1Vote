add75fdffba495a23a3a716c80ca41d282f9be78
mziaullah479-ops <mziaullah479@gmail.com>
2026-09-08 19:50:12 -0400

    Add optional Google Analytics measurement support

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,29 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+declare global {
+  interface Window {
+    dataLayer: unknown[][];
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID as string | undefined;
+
+if (measurementId) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(measurementId)}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function gtag(...args: unknown[]) {
+    window.dataLayer.push(args);
+  };
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId, { anonymize_ip: true });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
